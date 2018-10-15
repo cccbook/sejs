0d2961f01e70438616225a18fca7efde6d895a0e
ccckmit <ccckmit@gmail.com>
2018-10-15 10:29:28 +0800

    push origin master

--- 01-test/00-uselodash/main.js
+++ 01-test/00-uselodash/main.js
@@ -3,5 +3,5 @@ const _ = require('lodash')
 console.log("_.chunk(['a', 'b', 'c', 'd'], 2)=", _.chunk(['a', 'b', 'c', 'd'], 2))
 // => [['a', 'b'], ['c', 'd']]
  
-console.log("_.chunk(['a', 'b', 'c', 'd'], 3)", _.chunk(['a', 'b', 'c', 'd'], 3))
+console.log("_.chunk(['a', 'b', 'c', 'd'], 3)=", _.chunk(['a', 'b', 'c', 'd'], 3))
 // => [['a', 'b', 'c'], ['d']]
